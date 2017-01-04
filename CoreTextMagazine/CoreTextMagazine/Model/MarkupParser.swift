636ae1cf0e5fc52ff9679a1c8d71fed0b9c5dd24
LamLeVan <vanlam1503@gmail.com>
2017-01-05 01:17:45 +0700

    add nsregex

--- CoreTextMagazine/CoreTextMagazine/Model/MarkupParser.swift
+++ CoreTextMagazine/CoreTextMagazine/Model/MarkupParser.swift
@@ -9,9 +9,23 @@
 import UIKit
 import SwiftyJSON
 
+//Callbacks
+var ascentCallback = { (ref: [String: Any]) -> CGFloat in
+    if let height = ref["height"] as? NSString {
+        return CGFloat(height.floatValue)
+    }
+    return 0
+}
+
+
 struct Regex {
     static let font = "(.*?)(<[^>]+>|\\Z)"
-    static let color = "(?<=strokeColor=\")\\w+"
+    static let strokeColor = "(?<=strokeColor=\")\\w+"
+    static let color = "(?<=color=\")\\w+"
+    static let face = "(?<=face=\")[^\"]+"
+    static let width = "(?<=width=\")[^\"]+"
+    static let height = "(?<=height=\")[^\"]+"
+    static let src = "(?<=height=\")[^\"]+"
 }
 
 class MarkupParser: NSObject {
@@ -20,9 +34,9 @@ class MarkupParser: NSObject {
     var color: UIColor
     var strokeColor: UIColor
     var strokeWidth: Float
-    var images: [String] = []
+    var images: [[String: Any]] = []
     
-    init(color: UIColor, strokeColor: UIColor, strokeWidth: Float, images: [String]) {
+    init(color: UIColor, strokeColor: UIColor, strokeWidth: Float, images: [[String: Any]]) {
         self.font = "Arial"
         self.color = color
         self.strokeColor = strokeColor
@@ -52,30 +66,81 @@ class MarkupParser: NSObject {
                     let tag = parts[1]
                     if tag.hasPrefix("font") {
                         //stroke color
-                        let scolorRegex = try? NSRegularExpression(pattern: Regex.color, options: [])
-                        scolorRegex?.enumerateMatches(in: tag, options: [], range: NSMakeRange(0, tag.length), using: { (match, flags, stop) in
+                        let scolorRegex = try? NSRegularExpression(pattern: Regex.strokeColor, options: [])
+                        scolorRegex?.enumerateMatches(in: tag, options: [], range: NSMakeRange(0, tag.length), using: { [unowned self] (match, flags, stop) in
                             if let range = match?.range {
                                 if (tag as NSString).substring(with: range) == "none" {
                                     self.strokeWidth = 0.0
+                                } else {
+                                    self.strokeWidth = -3.0
+                                    let colorSel: Selector = NSSelectorFromString("\((tag as NSString).substring(with: range))Color")
+                                    self.color = UIColor.perform(colorSel)
                                 }
                             }
                         })
+                        
+                        //color
+                        let colorRegex = try? NSRegularExpression(pattern: Regex.color, options: NSRegularExpression.Options.caseInsensitive)
+                        colorRegex?.enumerateMatches(in: tag, options: [], range: NSMakeRange(0, tag.length), using: { [unowned self] (match, flags, stop) in
+                            if let range = match?.range {
+                                let colorSel: Selector = NSSelectorFromString("\((tag as NSString).substring(with: range))Color")
+                                self.color = UIColor.perform(colorSel)
+                            }
+                        })
+                        
+                        //face
+                        let faceRegex = try? NSRegularExpression(pattern: Regex.face, options: [])
+                        faceRegex?.enumerateMatches(in: tag, options: [], range: NSMakeRange(0, tag.length), using: { (match, flags, stop) in
+                            if let range = match?.range {
+                                self.font = (tag as NSString).substring(with: range)
+                            }
+                        })
+                        if tag.hasPrefix("img") {
+                            var width: NSNumber = NSNumber(integerLiteral: 0)
+                            var height: NSNumber = NSNumber(integerLiteral: 0)
+                            var fileName = ""
+                            let widthRegex = try? NSRegularExpression(pattern: Regex.width, options: [])
+                            widthRegex?.enumerateMatches(in: tag, options: [], range: NSMakeRange(0, tag.length), using: { [unowned self] (match, flags, stop) in
+                                if let range = match?.range {
+                                    width = NSNumber(integerLiteral: (tag as NSString).substring(with: range).intValue)
+                                }
+                            })
+                            
+                            //height
+                            let heightRegex = try? NSRegularExpression(pattern: Regex.height, options: [])
+                            heightRegex?.enumerateMatches(in: tag, options: [], range: NSMakeRange(0, tag.length), using: { (match, flags, stop) in
+                                if let range = match?.range {
+                                    height = NSNumber(integerLiteral: (tag as NSString).substring(with: range).intValue)
+                                }
+                            })
+                            
+                            //image
+                            let srcRegex = try? NSRegularExpression(pattern: Regex.src, options: [])
+                            srcRegex?.enumerateMatches(in: tag, options: [], range: NSMakeRange(0, tag.length), using: { (match, flags, stop) in
+                                if let range = match?.range {
+                                    fileName = (tag as NSString).substring(with: range)
+                                }
+                            })
+                            
+                            //add the image for drawing
+                            let dict: [String: Any] = [
+                                "width": width,
+                                "height": height,
+                                "fileName": fileName,
+                                "location": NSNumber(integerLiteral: aString.length)
+                            ]
+                            self.images.append(dict)
+                            
+                            //render empty space for drawing the image in the text
+                            let callbacks: CTRunDelegateCallbacks
+                            callbacks.version = kCTRunDelegateVersion1
+                            callbacks.getAscent = ascentCallback
+                        }
                     }
                 }
             }
         }
         
     }
-}
-
-// MARK: Conform CFString is hashable
-extension CFString: Hashable {
-    public var hashValue: Int {
-        return self.hashValue
-    }
-}
-
-public func ==(lhs: CFString, rhs: CFString) -> Bool { // Cast CFString to String is to prevent endless
-    return (lhs as String) == (rhs as String)
 }
 
