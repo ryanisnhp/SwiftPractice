7d8a9315400eafbe71121785d3aeebc555eb04d3
at-lamlv <lamlv@asiantech.vn>
2017-01-27 22:28:18 +0700

    creationPattern-configWindow

---
+++ Simple DesignPattern/CreationPattern/CreationPattern/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CreationPattern
+//
+//  Created by Lam Le Van on 1/27/17.
+//  Copyright © 2017 dev.asiantech.lamlv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
