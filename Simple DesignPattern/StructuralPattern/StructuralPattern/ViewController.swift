ccd7749204d3a6364832ef8ab22db8e15b6e0935
at-lamle <lamle@asiantech.vn>
2017-01-31 21:55:08 +0700

    Make file

---
+++ Simple DesignPattern/StructuralPattern/StructuralPattern/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StructuralPattern
+//
+//  Created by Lam Le Van on 1/31/17.
+//  Copyright © 2017 dev.asiantech.lamlv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
