82e5139aad4c4093e5f1b667ae9ae209bc9839d6
at-lamle <lamle@asiantech.vn>
2017-02-04 10:44:20 +0700

    proxy pattern

--- Simple DesignPattern/StructuralPattern/StructuralPattern/ProxyPattern.swift
+++ Simple DesignPattern/StructuralPattern/StructuralPattern/ProxyPattern.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+protocol IAnimation {
+    func display() -> String
+    func click() -> String
+}
+
+class Video: IAnimation {
+    
+    func click() -> String {
+        return ""
+    }
+    
+    func display() -> String {
+        return "Display the video"
+    }
+    
+    func load() -> String {
+        return "Loading the video"
+    }
+    
+    func play() -> String {
+        return "Playing the video"
+    }
+}
+
+class ScreenShot: IAnimation {
+    
+    private var video: Video?
+    
+    func display() -> String {
+        if let video = video {
+            return video.display()
+        } else {
+            return "Display the screen shot of the video"
+        }
+    }
+
+    func click() -> String {
+        if let video = video {
+            return video.play()
+        } else {
+            video = Video()
+            return video!.load()
+        }
+    }
+}
