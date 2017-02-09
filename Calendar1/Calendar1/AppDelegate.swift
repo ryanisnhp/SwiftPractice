aa3c598882721897895994ca708f2ccff8d571de
at-lamle <lamle@asiantech.vn>
2017-02-09 22:14:43 +0700

    Make project

---
+++ Calendar1/Calendar1/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Calendar1
+//
+//  Created by Lam Le Van on 2/10/17.
+//  Copyright © 2017 dev.asiantech.lamlv. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    private func configWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.rootViewController = ViewController()
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
---
+++ Calendar1/Calendar1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Calendar1
+//
+//  Created by Lam Le Van on 2/10/17.
+//  Copyright © 2017 dev.asiantech.lamlv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
