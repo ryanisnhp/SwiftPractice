857f6a06ec77b69e6838e6174d953ced2c9960ea
LamLeVan <vanlam1503@gmail.com>
2017-02-18 22:50:50 +0700

    pod install and add network model

---
+++ MapDemo/HonoluluArtDemo/HonoluluArtDemo/BasicNetwork.swift
@@ -0,0 +1,53 @@
+//
+//  BasicNetwork.swift
+//  AlamofireDemo
+//
+//  Created by LeVanLam on 2/15/17.
+//  Copyright © 2017 LeVanLam. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+protocol BasicNetworkable {
+    var urlString: String {get}
+    var method: HTTPMethod {get}
+    var parameter: [String: Any] {get}
+}
+
+class BasicNetwork: BasicNetworkable {
+    
+    required init() {
+    }
+    internal var method: HTTPMethod {
+        fatalError("method is not overriden")
+    }
+
+    internal var parameter: [String : Any] {
+        fatalError("parameter is not overriden")
+    }
+
+    var urlString: String {
+        fatalError("urlString is not overriden")
+    }
+    
+    final private var contentType: [String] {
+        return ["application/json"]
+    }
+    
+    func execute() {
+        Alamofire.request(urlString, method: method, parameters: parameter, encoding: URLEncoding.httpBody).validate(statusCode: 200..<300).validate(contentType: contentType).responseJSON { (response) in
+            switch response.result {
+            case .success(let value):
+                print(value)
+                self.connectDidFinishWith(json: JSON(value))
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+    
+    func connectDidFinishWith(json: JSON) {}
+}
