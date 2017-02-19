a515f81894f6f120a7d931411a8f2a07f2c3db6e
LamLeVan <vanlam1503@gmail.com>
2017-02-19 10:42:59 +0700

    add google api key

---
+++ MapDemo/HonoluluArtDemo2/HonoluluArtDemo2/Helpers.swift
@@ -0,0 +1,13 @@
+//
+//  Helpers.swift
+//  HonoluluArtDemo2
+//
+//  Created by LeVanLam on 2/19/17.
+//  Copyright © 2017 LeVanLam. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    static let googleKey = "AIzaSyBr2ikZPW108eFNJKYsU6_DfjEC_NVpdEI"
+}
