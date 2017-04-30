bf6cd411fe01fb4814b2bb251cee7c1b367db344
at-lamle <lam.le@asiantech.vn>
2017-04-30 10:52:33 +0700

    make project

---
+++ CoreData/Bowties/Bowties/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Bowties
+//
+//  Created by Le Van Lam on 4/30/17.
+//  Copyright © 2017 LeVanLam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak fileprivate var segmentedControl: UISegmentedControl!
+    @IBOutlet weak fileprivate var imageView: UIImageView!
+    @IBOutlet weak fileprivate var nameLabel: UILabel!
+    @IBOutlet weak fileprivate var ratingLabel: UILabel!
+    @IBOutlet weak fileprivate var timesWornLabel: UILabel!
+    @IBOutlet weak fileprivate var lastWornLabel: UILabel!
+    @IBOutlet weak fileprivate var favoriteLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func segmentedControl(control: UISegmentedControl) {
+        
+    }
+    @IBAction func wear(sender: UIButton) {
+        
+    }
+    @IBAction func rate(sender: UIButton) {
+        
+    }
+}
